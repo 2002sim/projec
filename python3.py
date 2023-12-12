7a11a3bd76624fc39f45bc601b3fd683962fcea0
2002sim <133947569+2002sim@users.noreply.github.com>
2023-12-12 18:37:08 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+8794383irehlfkdsj
+svsuihdkkfhdsj
+skeye8ry4o3ryiuewkjf
